87afdd30b11aa43a13a844abf0ad887ead5b2da3
James Rao <55598464+JamesRao98@users.noreply.github.com>
2021-12-13 15:03:52 -0500

    more stoopid fixes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
-const app = require("express")();
+const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
 
 app.use(cors());
+app.use(express.json());
+app.use(morgan("combined"));
 
 app.post("/", async (req, res) => {
     try {
-        res.json(await (await axios(req.body)).json());
+        let resp = await axios(req.body);
+        res.json(resp.data);
     } catch (error) {
         if(error.message) {
             res.status(400).send(error.message);
